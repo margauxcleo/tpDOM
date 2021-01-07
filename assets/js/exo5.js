2a4a0285fafbe34c8a37fa259b4fc3b0c2947352
Margaux Vitez <margaux.vitez@hotmail.fr>
2021-01-07 17:53:56 +0100

    add exo5 files

---
+++ assets/js/exo5.js
@@ -0,0 +1,20 @@
+function controler() {
+    // on crée la variable zoneSaisie, que l'on fait correspond à l'élément HTML ayant l'id zoneSaisie
+    var zoneSaisie = document.getElementById("zoneSaisie");
+    if (zoneSaisie.value) {
+        // on crée une alerte qui affiche le contenu de cette zone 
+        alert("La Zone de saisie contient : " + zoneSaisie.value);
+    }
+    else {
+        alert("La zone de saisie est vide !");
+    }  
+}
+
+// en cours partie 5 
+
+function eval(x) { 
+    var regex = #[A-Z0-9][+][A-Z0-9]#;
+    var x;
+    var y;
+    if ()
+}
