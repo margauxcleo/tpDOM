d8d6d17849b819375e60914b7b1b9178961e1059
Margaux Vitez <margaux.vitez@hotmail.fr>
2021-01-07 14:40:28 +0100

    add exo1 files

---
+++ assets/js/exo1.js
@@ -0,0 +1,14 @@
+// // Exo 1 question 1
+// function changer_style(){
+//     var x = document.getElementById("parag1");
+//     x.style.color = "white";
+//     x.style.backgroundColor = "black";
+//     x.style.border = "dashed black 1px";
+//     x.style.padding = "5px";
+// }
+
+// Exo 1 question 2
+function changer_style(){
+    var x = document.getElementById("parag1");
+    x.classList.add("active");
+}
